ecc9bdd32cb4f03f18f796917b6a0e4c73559353
AndroidParanoid <90028731+AndroidParanoi@users.noreply.github.com>
2021-09-06 20:07:25 +0100

    Update mirror.go

--- mirror.go
+++ mirror.go
@@ -19,41 +19,43 @@ func CheckContains(url_t string) bool {
 	return matched
 }
 
-func CheckReflectedParameters(url_t string, parameters url.Values, wg *sync.WaitGroup, sem chan bool) {
-	defer wg.Done()
-	<-sem
-	payloads := url.Values{}
-	i := 0
-	reversed_payload := make(map[string]string)
+func SetPayloads(parameters url.Values, reversed_payload map[string]string, payloads url.Values, i int) {
 	for name := range parameters {
 		payload := "swagonlolnow" + fmt.Sprint(i)
 		payloads.Set(name, payload)
 		reversed_payload[payload] = name
 		i++
 	}
-	var new_url string
-	encoded_payloads := payloads.Encode()
-	if CheckContains(url_t) {
-		new_url = fmt.Sprintf("%s&%s", url_t, encoded_payloads)
-	} else {
-		new_url = fmt.Sprintf("%s?%s", url_t, encoded_payloads)
-	}
+}
+
+func EncodePayloads(payloads url.Values) string {
+	return payloads.Encode()
+}
+
+func SendHttpRequestReadResponseBody(new_url string) []byte {
 	resp, err := http.Get(new_url)
 	if err != nil {
-		return
+		return nil
 	}
 	defer resp.Body.Close()
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return
+		return nil
 	}
-	bodyString := string(bodyBytes)
+	return bodyBytes
+}
+
+func CheckMatches(bodyString string, i int) []string {
 	var matches []string
 	for x := 0; x <= i; x++ {
 		pattern := "swagonlolnow" + fmt.Sprint(x)
 		re, _ := regexp.Compile(pattern)
 		matches = append(matches, re.FindString(bodyString))
 	}
+	return matches
+}
+
+func FindPayloadInReversePayloads(matches []string, reversed_payload map[string]string) url.Values {
 	reflected_url := url.Values{}
 	for payload, name := range reversed_payload {
 		for _, match := range matches {
@@ -62,14 +64,43 @@ func CheckReflectedParameters(url_t string, parameters url.Values, wg *sync.Wait
 			}
 		}
 	}
+	return reflected_url
+
+}
+
+func PrintReflections(reflected_url url.Values, new_url string, url_t string) {
 	if len(reflected_url) > 0 {
 		encoded_reflected_payloads := reflected_url.Encode()
-		new_url = strings.Split(url_t, "?")[0]
-		new_url = fmt.Sprintf("%s?%s", new_url, encoded_reflected_payloads)
-		fmt.Println(new_url)
+		url := strings.Split(url_t, "?")[0]
+		url = fmt.Sprintf("%s?%s", url, encoded_reflected_payloads)
+		fmt.Println(url)
 	} else {
 		return
 	}
+}
+
+func CheckReflectedParameters(url_t string, parameters url.Values, wg *sync.WaitGroup, sem chan bool) {
+	defer wg.Done()
+	<-sem
+	i := 0
+	payloads := url.Values{}
+	reversed_payload := make(map[string]string)
+	SetPayloads(parameters, reversed_payload, payloads, i)
+	encoded_payloads := EncodePayloads(payloads)
+	var new_url string
+	if CheckContains(url_t) {
+		new_url = fmt.Sprintf("%s&%s", url_t, encoded_payloads)
+	} else {
+		new_url = fmt.Sprintf("%s?%s", url_t, encoded_payloads)
+	}
+	bodyBytes := SendHttpRequestReadResponseBody(new_url)
+	if bodyBytes != nil {
+		return
+	}
+	bodyString := string(bodyBytes)
+	matches := CheckMatches(bodyString, i)
+	reflected_url := FindPayloadInReversePayloads(matches, reversed_payload)
+	PrintReflections(reflected_url, new_url, url_t)
 }
 
 func main() {
